edf951657428e583d08d272f1089305432a33b91
savio-2-lopes <savioaugulopes@gmail.com>
2021-02-24 23:31:19 -0300

    :rocket: Update lesson three

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/components/Profile.module.css'
 export function Profile() {
     return (
         <div className={styles.profileContainer}>
+
             <img src="https://github.com/savio-2-lopes.png" alt="Savio Lopes" />
+
             <div>
                 <strong> Savio Lopes</strong>
                 <p>
