36e9ddde0ad98a557cc06a3c647afbce321a0b33
savio-2-lopes <savioaugulopes@gmail.com>
2021-02-25 23:47:11 -0300

    :dart: Update lesson four

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
 
@@ -9,7 +13,7 @@ export function Profile() {
             <div>
                 <strong> Savio Lopes</strong>
                 <p>
-                    <img src="icons/level.svg" alt="Level" />Level 1
+                    <img src="icons/level.svg" alt="Level" />Level {level}
                 </p>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { ChallengeBox } from '../components/ChallengeBox';
 import { Profile } from '../components/Profile'
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return (
@@ -17,18 +18,19 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
 
-        <div>
-          <ChallengeBox />
-        </div>
-
-      </section>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div >
   )
 }
