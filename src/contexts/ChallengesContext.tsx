70efb59b9a2eaae677503169d41c6a316924c516
savio-2-lopes <savioaugulopes@gmail.com>
2021-02-25 22:07:08 -0300

    :rocket: Update lesson three

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,21 @@
 import { createContext, ReactNode, useState } from 'react'
+import challenges from '../../challenges.json'
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: number;
+    amount: number
+}
 
 interface ChallengesContextData {
     level: number;
+    experienceToNextLevel: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
+    resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -18,13 +28,23 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0)
+    const [activeChallenge, setActiveChallenge] = useState(null)
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
     function levelUp() {
         setLevel(level + 1)
     }
 
     function startNewChallenge() {
-        console.log('New Challenge')
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const challenge = challenges[randomChallengeIndex]
+
+        setActiveChallenge(challenge)
+    }
+
+    function resetChallenge() {
+        setActiveChallenge(null)
     }
 
     return (
@@ -32,9 +52,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
             value={{
                 level,
                 currentExperience,
+                experienceToNextLevel,
                 challengesCompleted,
                 levelUp,
-                startNewChallenge
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge
             }}
         >
             {children}
